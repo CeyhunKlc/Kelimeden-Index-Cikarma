9880dddee2538cee57d42505799572e098b97956
CeyhunKlc <117180182+CeyhunKlc@users.noreply.github.com>
2022-12-20 16:54:17 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace algoritma
+{ 
+    class program
+    { 
+        static void Main(string[]args)
+        { 
+            Console.WriteLine("Lütfen Kelime,Sayı Giriniz:");
+            string input = Console.ReadLine();
+
+            string[] input1= input.Split(",");
+            string Kelime= input1[0];
+            int cevir= Convert.ToInt32(input1[1]);
+            string sonhali= Kelime.Remove(cevir-1,1);
+            Console.WriteLine(sonhali);
+        }
+    }
+}
